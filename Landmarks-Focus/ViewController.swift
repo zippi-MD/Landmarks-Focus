0d9649a3d442eaebce3a04391a71a3e9ac6028dc
Alejandro Mendoza <zippi180@gmail.com>
2019-04-25 19:01:37 -0500

    Initial Commit

---
+++ Landmarks-Focus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmarks-Focus
+//
+//  Created by Alejandro Mendoza on 4/25/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
